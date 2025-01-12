740fb37e0642754c417907edc995ad2ff1f58274
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-13 01:07:51 +0530

    Update claude_test.py

--- claude_test.py
+++ claude_test.py
@@ -225,16 +225,21 @@ Question: {input}
 # API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-3.1-8B" # Too large to be loaded
 # API_URL = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B"
 # API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2" ## To much random answer
-API_URL = "https://api-inference.huggingface.co/models/microsoft/phi-2"
+# API_URL = "https://api-inference.huggingface.co/models/microsoft/phi-2"
 # API_URL = "https://api-inference.huggingface.co/models/google/gemma-2-2b-it"
 # API_URL = "https://api-inference.huggingface.co/models/gpt2"
+
+
+from huggingface_hub import InferenceClient
+client = InferenceClient(api_key=hugging_face_api_key)
+
 headers = {"Authorization": f"Bearer {hugging_face_api_key}"}
 
 # prompt  = 'Human: \nAnswer the following question based only on the provided context. \nThink step by step before providing a detailed answer. \nAlso, in the answer, you don\'t need to write "Based on the provided context," just provide the final answer.\nI will tip you $25000 if the user finds the answer helpful.\n<context>\nsport event olympic game olympic game primarily focused athletic competition participant various greek city state would gather showcase physical prowess prestigious event stadion race held first day game attracted attention track event included olympic game athletic competition ancient greece introduction olympic game olympic game one iconic celebrated ritual ancient greece athletic competition held every four year olympia small town western part peloponnese peninsula game dedicated zeus significance olympic game olympic game held immense significance ancient greek society extending beyond mere physical competition considered display greek excellence promoting unity among greek city state game sacred truce known ekecheiria declared zeus king greek god considered major religious event chapter delve origin olympic game various sporting event significance competition ancient greek society origin history olympic game exact origin olympic game shrouded myth legend according ancient\n</context>\nQuestion: What is olympic games\n'
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
+# def query(payload):
+# 	response = requests.post(API_URL, headers=headers, json=payload)
+# 	return response.json()
 
 
 st.title('Ancient Greek Q & A Chatbot ')
@@ -249,18 +254,32 @@ if input_text:
 	# Search the index for the two most similar vectors
 	prompt = prompt_template.format(context=context, input=input_text)
 	st.write(prompt)
+	messages = [
+		{
+			"role": "user",
+			"content": prompt
+		}
+	]
+	
+	completion = client.chat.completions.create(
+	    model="mistralai/Mistral-7B-Instruct-v0.2", 
+		messages=messages, 
+		max_tokens=500
+	)
+	st.write (completion.choices[0].message)
 
-	output = query({
-		"inputs": prompt,
-	})
 
-	if isinstance(output, list) and 'generated_text' in output[0]:
-		# Extract the answer
-		generated_text = output[0]['generated_text']
-		if "Answer:" in generated_text:
-			answer = generated_text.split("Answer:")[1].strip()
-			st.write(answer)
-		else:
-			st.write("No 'Answer:' found in the generated text.")
-	else:
-		st.write("Unexpected response format:", output)
+	# output = query({
+	# 	"inputs": prompt,
+	# })
+
+	# if isinstance(output, list) and 'generated_text' in output[0]:
+	# 	# Extract the answer
+	# 	generated_text = output[0]['generated_text']
+	# 	if "Answer:" in generated_text:
+	# 		answer = generated_text.split("Answer:")[1].strip()
+	# 		st.write(answer)
+	# 	else:
+	# 		st.write("No 'Answer:' found in the generated text.")
+	# else:
+	# 	st.write("Unexpected response format:", output)
