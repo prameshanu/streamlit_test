450d4468fc48d7c0a6b15710341be8f974f8c7d1
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-12 23:10:05 +0530

    Update claude_test.py

--- claude_test.py
+++ claude_test.py
@@ -11,4 +11,5 @@ hugging_face_api_key = st.secrets["HUGGING_FACE_API_KEY"]
 
 st.title('Test')
 st.write('PINE_CONE_API: ' , pine_cone_api_key)
+st.write('hugging_face_api_key: ' , hugging_face_api_key)
 
