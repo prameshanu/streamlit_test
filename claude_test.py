10da47ea26e781d080cbc5bb887fdcfd33c67035
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-12 23:11:39 +0530

    Update claude_test.py

--- claude_test.py
+++ claude_test.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
 
+import requests
+
+
 
 pine_cone_api_key = st.secrets["PINE_CONE_API_KEY"]
 claude_api_key = st.secrets["CLAUDE_API_KEY"]
@@ -9,7 +12,20 @@ langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
 hugging_face_api_key = st.secrets["HUGGING_FACE_API_KEY"]
 
 
+
+API_URL = "https://api-inference.huggingface.co/models/google/gemma-2-2b-it"
+headers = {"Authorization": f"Bearer {hugging_face_api_key}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": prompt,
+})
+
+
 st.title('Test')
 st.write('PINE_CONE_API: ' , pine_cone_api_key)
 st.write('hugging_face_api_key: ' , hugging_face_api_key)
-
+st.write('answer', output)
