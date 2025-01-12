b7aeb653cbd9fa1717094b2c1c20e55e3e7fdf12
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-12 23:08:10 +0530

    Update claude_test.py

--- claude_test.py
+++ claude_test.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
+
+
+pine_cone_api_key = st.secrets["PINE_CONE_API_KEY"]
+claude_api_key = st.secrets["CLAUDE_API_KEY"]
+
+langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
+hugging_face_api_key = st.secrets["HUGGING_FACE_API_KEY"]
+
+
 st.title('Test')
+st.write('PINE_CONE_API: ' pine_cone_api_key)
 
