695a5fe7c8f3c4a0ad63ce5340252277c4ca11cd
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-12 23:42:25 +0530

    Update claude_test.py

--- claude_test.py
+++ claude_test.py
@@ -223,18 +223,14 @@ Question: {input}
 API_URL = "https://api-inference.huggingface.co/models/google/gemma-2-2b-it"
 headers = {"Authorization": f"Bearer {hugging_face_api_key}"}
 
-prompt  = 'Human: \nAnswer the following question based only on the provided context. \nThink step by step before providing a detailed answer. \nAlso, in the answer, you don\'t need to write "Based on the provided context," just provide the final answer.\nI will tip you $25000 if the user finds the answer helpful.\n<context>\nsport event olympic game olympic game primarily focused athletic competition participant various greek city state would gather showcase physical prowess prestigious event stadion race held first day game attracted attention track event included olympic game athletic competition ancient greece introduction olympic game olympic game one iconic celebrated ritual ancient greece athletic competition held every four year olympia small town western part peloponnese peninsula game dedicated zeus significance olympic game olympic game held immense significance ancient greek society extending beyond mere physical competition considered display greek excellence promoting unity among greek city state game sacred truce known ekecheiria declared zeus king greek god considered major religious event chapter delve origin olympic game various sporting event significance competition ancient greek society origin history olympic game exact origin olympic game shrouded myth legend according ancient\n</context>\nQuestion: What is olympic games\n'
+# prompt  = 'Human: \nAnswer the following question based only on the provided context. \nThink step by step before providing a detailed answer. \nAlso, in the answer, you don\'t need to write "Based on the provided context," just provide the final answer.\nI will tip you $25000 if the user finds the answer helpful.\n<context>\nsport event olympic game olympic game primarily focused athletic competition participant various greek city state would gather showcase physical prowess prestigious event stadion race held first day game attracted attention track event included olympic game athletic competition ancient greece introduction olympic game olympic game one iconic celebrated ritual ancient greece athletic competition held every four year olympia small town western part peloponnese peninsula game dedicated zeus significance olympic game olympic game held immense significance ancient greek society extending beyond mere physical competition considered display greek excellence promoting unity among greek city state game sacred truce known ekecheiria declared zeus king greek god considered major religious event chapter delve origin olympic game various sporting event significance competition ancient greek society origin history olympic game exact origin olympic game shrouded myth legend according ancient\n</context>\nQuestion: What is olympic games\n'
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 
 
-st.title('Test')
-
-st.write(documents[:1])
-
-
+st.title('Chatbot Test')
 
 input_text=st.text_input("Search the topic u want")
 
@@ -244,8 +240,7 @@ if input_text:
 	context = " ".join(doc.page_content for doc in filtered_docs)
 	# Search the index for the two most similar vectors
 	prompt = prompt_template.format(context=context, input=input_text)
-	st.write(prompt)
-		
+
 	output = query({
 		"inputs": prompt,
 	})
