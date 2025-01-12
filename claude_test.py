17aae55b2fffd8079a6afacd8281b057c16df20d
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-12 23:34:15 +0530

    Update claude_test.py

--- claude_test.py
+++ claude_test.py
@@ -1,7 +1,66 @@
 import streamlit as st
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings  # Replace with appropriate embedding
+from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_community.retrievers import PineconeHybridSearchRetriever
+import sentence_transformers
+from langchain.chains import RetrievalQA
+from concurrent.futures import ThreadPoolExecutor
+from langchain_huggingface import HuggingFaceEmbeddings
+import anthropic
+
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from pinecone_text.sparse.bm25_encoder import BM25Encoder
 
 
+import warnings
+import streamlit as st
+
+import numpy as np
+from dotenv import load_dotenv
+
+import os
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.data import find
 import requests
+
+
+# ## lazy loading
+try:
+    # Check if 'punkt' is available; download if not
+    find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    # Check if 'stopwords' is available; download if not
+    find('corpora/stopwords.zip')
+except LookupError:
+    nltk.download('stopwords')
+
+try:
+    # Check if 'wordnet' is available; download if not
+    find('corpora/wordnet.zip')
+except LookupError:
+    nltk.download('wordnet')
+
+try:
+    # Check if 'punkt_tab' is available; download if not
+    find('corpora/punkt_tab.zip')
+except LookupError:
+    nltk.download('punkt_tab')
 
 
 
@@ -10,6 +69,7 @@ claude_api_key = st.secrets["CLAUDE_API_KEY"]
 
 langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
 hugging_face_api_key = st.secrets["HUGGING_FACE_API_KEY"]
+
 
 
 
