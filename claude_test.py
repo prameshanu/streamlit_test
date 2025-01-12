f38947fc41916e927d920e8f5e1bfefe2e73a3d7
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-13 01:12:14 +0530

    Update claude_test.py

--- claude_test.py
+++ claude_test.py
@@ -19,6 +19,7 @@ import anthropic
 from pinecone import Pinecone
 from pinecone import ServerlessSpec
 from pinecone_text.sparse.bm25_encoder import BM25Encoder
+from huggingface_hub import InferenceClient
 
 
 import warnings
@@ -230,16 +231,10 @@ Question: {input}
 # API_URL = "https://api-inference.huggingface.co/models/gpt2"
 
 
-from huggingface_hub import InferenceClient
+
 client = InferenceClient(api_key=hugging_face_api_key)
 
 headers = {"Authorization": f"Bearer {hugging_face_api_key}"}
-
-# prompt  = 'Human: \nAnswer the following question based only on the provided context. \nThink step by step before providing a detailed answer. \nAlso, in the answer, you don\'t need to write "Based on the provided context," just provide the final answer.\nI will tip you $25000 if the user finds the answer helpful.\n<context>\nsport event olympic game olympic game primarily focused athletic competition participant various greek city state would gather showcase physical prowess prestigious event stadion race held first day game attracted attention track event included olympic game athletic competition ancient greece introduction olympic game olympic game one iconic celebrated ritual ancient greece athletic competition held every four year olympia small town western part peloponnese peninsula game dedicated zeus significance olympic game olympic game held immense significance ancient greek society extending beyond mere physical competition considered display greek excellence promoting unity among greek city state game sacred truce known ekecheiria declared zeus king greek god considered major religious event chapter delve origin olympic game various sporting event significance competition ancient greek society origin history olympic game exact origin olympic game shrouded myth legend according ancient\n</context>\nQuestion: What is olympic games\n'
-
-# def query(payload):
-# 	response = requests.post(API_URL, headers=headers, json=payload)
-# 	return response.json()
 
 
 st.title('Ancient Greek Q & A Chatbot ')
@@ -253,7 +248,6 @@ if input_text:
 	context = " ".join(doc.page_content for doc in filtered_docs)
 	# Search the index for the two most similar vectors
 	prompt = prompt_template.format(context=context, input=input_text)
-	st.write(prompt)
 	messages = [
 		{
 			"role": "user",
@@ -266,20 +260,5 @@ if input_text:
 		messages=messages, 
 		max_tokens=500
 	)
-	st.write (completion.choices[0].message)
+	st.write (completion.choices[0].message.content)
 
-
-	# output = query({
-	# 	"inputs": prompt,
-	# })
-
-	# if isinstance(output, list) and 'generated_text' in output[0]:
-	# 	# Extract the answer
-	# 	generated_text = output[0]['generated_text']
-	# 	if "Answer:" in generated_text:
-	# 		answer = generated_text.split("Answer:")[1].strip()
-	# 		st.write(answer)
-	# 	else:
-	# 		st.write("No 'Answer:' found in the generated text.")
-	# else:
-	# 	st.write("Unexpected response format:", output)
